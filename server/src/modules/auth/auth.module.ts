b48dc514f43888c5a13cc6c1aa9ae626054f2b69
Bernardo <bernardo.soares30@outlook.com>
2026-05-15 10:47:22 -0300

    fix: cookie fix

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -15,7 +15,9 @@ import { HashModule } from '../../common/hash/hash.module';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') ?? '1d' },
+        signOptions: {
+          expiresIn: configService.get<`${number}${'s' | 'm' | 'h' | 'd'}`>('JWT_EXPIRES_IN') ?? '1d',
+        },
       }),
     }),
   ],
