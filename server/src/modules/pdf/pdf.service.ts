ecba7644d2c7e3568e03fd33c69712a11640f786
Bernardo Soares <bernardo.soares30@outlook.com>
2026-06-09 20:33:54 -0300

    fix: pdf fix

--- server/src/modules/pdf/pdf.service.ts
+++ server/src/modules/pdf/pdf.service.ts
@@ -3,7 +3,6 @@ import * as fs from 'fs';
 import * as path from 'path';
 import puppeteer from 'puppeteer';
 import { buildPlanHtml, type PlanPdfData } from './templates/plan.template';
-import { buildWeeklyReportHtml, type WeeklyReportPdfData } from './templates/weekly-report.template';
 
 @Injectable()
 export class PdfService {
