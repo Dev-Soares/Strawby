f283e6367fb8004d276d361ac0727f89b1b01bd1
Bernardo <bernardo.soares30@outlook.com>
2026-06-11 12:33:17 -0300

    chore: firebase initial config done

---
+++ client/src/firebase/config/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCzQ8w0fG7GvesH7hMdYBaLJfgQGADwPeM",
+  authDomain: "strawby-548b5.firebaseapp.com",
+  projectId: "strawby-548b5",
+  storageBucket: "strawby-548b5.firebasestorage.app",
+  messagingSenderId: "413731395744",
+  appId: "1:413731395744:web:e2f3c4cf6b1fd758a651c5",
+  measurementId: "G-C4X3J812T0"
+};
